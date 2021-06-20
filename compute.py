f1ab4d6631a6eb4e21b4681f67e96e010e29a8ae
Ameliorate <git-spam-or-if-human-remove-this-first-part-amelorate@amel.pw>
2021-06-19 22:28:56 -0400

    Add time estimate

--- compute.py
+++ compute.py
@@ -5,6 +5,7 @@ import os
 from sys import stderr
 from collections import deque
 from copy import deepcopy
+from math import dist
 import json
 
 datadir = r'./data'
@@ -65,10 +66,14 @@ def route(from_canon, to_canon):
             dest = []
             advisory = {}
             current = current_node
+            time_min1 = 0
+            time_max1 = 0
             while current is not None:
                 canon_dest = current.canonical_destination
                 if current.parent is not None:
                     canon_dest = current.parent.data['BadLinks'].get(current.canonical_destination) or current.canonical_destination
+                    time_min1 += time_min(current.parent.position[0], current.parent.position[1], current.position[0], current.position[1])
+                    time_max1 += time_max(current.parent.position[0], current.parent.position[1], current.position[0], current.position[1])
                 dest.append(canon_dest)
                 if 'advisory' in current.data:
                     advisory[current.canonical_destination] = current.data['advisory']
@@ -76,7 +81,7 @@ def route(from_canon, to_canon):
             dest_command = ' '.join(dest[::-1])
             if data[to_canon].get('station'):
                 dest_command = dest_command + f" {to_canon}:exit"
-            return { 'dest': dest_command, 'from': from_canon, 'to': to_canon, 'advisory': advisory }
+            return { 'dest': dest_command, 'from': from_canon, 'to': to_canon, 'advisory': advisory, 'time_min': time_min1, 'time_max': time_max1 }
         links = deque([])
         for link in current_node.data['links']:
             new_node = Node(current_node, link)
@@ -93,6 +98,18 @@ def route(from_canon, to_canon):
             if len([open_node for open_node in open_list if link.canonical_destination == open_node.canonical_destination and link.g > open_node.g]) > 0:
                 continue
             open_list.append(link)
+
+def time_min(from_x, from_z, to_x, to_z):
+    blocks_per_second = 11.314 # max speed on a diagonal rail
+    distance = dist((from_x, from_z), (to_x, to_z))
+    time_seconds = distance / blocks_per_second
+    return round(time_seconds)
+
+def time_max(from_x, from_z, to_x, to_z):
+    blocks_per_second = 6 # guess at acceptable speed on a straight rail
+    distance = abs(from_x - to_x) + abs(from_z - to_z) # taxicab geometry distance, assumes rail goes to first coordinate in a straight line then makes a right angle to go to the other coordinate in a straight line
+    time_seconds = distance / blocks_per_second
+    return round(time_seconds)
 
 routeData = []
 
