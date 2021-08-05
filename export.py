452a6a207626a4d1cabb3531720b1213a61b18e4
Ameliorate <git-spam-or-if-human-remove-this-first-part-amelorate@amel.pw>
2021-08-05 13:15:06 -0400

    Add simple data export program

---
+++ export.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import toml
+import os
+from sys import stderr
+import json
+
+datadir = r'./data'
+
+data = {}
+
+def processFile(path, name):
+    print(f"Processing file {path}", file=stderr)
+    name = name.removesuffix('.toml')
+    myData = toml.load(path)
+    if 'links' not in myData:
+        myData['links'] = []
+    if 'BadLinks' not in myData:
+        myData['BadLinks'] = {}
+    data[name] = myData
+
+for subdir, dirs, files in os.walk(datadir):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+
+        if filepath.endswith('.toml'):
+            processFile(filepath, filename)
+
+exported = json.dumps(data, indent="\t")
+print(exported)
